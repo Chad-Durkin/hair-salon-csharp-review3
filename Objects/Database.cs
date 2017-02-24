1d0a75127340acfc16ef0f2f99b79f749562e776
Chad Durkin <chad@>
2017-02-24 08:10:04 -0800

    add almost full files setup create Database Object with connections

---
+++ Objects/Database.cs
@@ -0,0 +1,27 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace HairSalon
+{
+    public class DB
+    {
+        public static SqlConection Connection()
+        {
+            SqlConnection conn = new SqlConnection(DBConfiguration.ConnectionString);
+        }
+
+        public static void CloseSqlConnections(SqlDataReader reader, SqlConnection connection)
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
+        
+    }
+}
